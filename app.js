e86404b7db982857d95f7f7ff30c2d92924b46b5
Alin Georgian Antoci <alin.georgian.antoci@gmail.com>
2019-04-22 15:20:42 +0300

    registration finished

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -23,7 +25,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+//  Express session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//  Connect flash
+app.use(flash());
+
+//  Global vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 //  Routes
 app.use('/', require('./routes/index'));
